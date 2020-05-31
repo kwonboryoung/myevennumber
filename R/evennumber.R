be8ad45a1ed15fd0b753d5fc00c30baa49437ed2
kwonboryoung <s21810696@stat01.yu.ac.kr>
2020-05-31 16:51:00 +0900

    determine even number

---
+++ R/evennumber.R
@@ -0,0 +1,17 @@
+#' Even number determination
+#'
+#' This function determines whether the value is even.
+#'
+#' @examples
+#'
+#' myfunction(10)
+myfunction <- function(x){
+
+  if(x %% 2 == 0)
+    print("짝수입니다.")
+  else
+    ("짝수가 아닙니다.")
+
+
+}
+
